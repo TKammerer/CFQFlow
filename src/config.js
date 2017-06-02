7ff20dda91bc513524618ebb5a7a61592a25fb75
Trent Kammerer <trent.kammerer@aarons.com>
2017-06-02 14:29:40 -0400

    testing

--- src/config.js
+++ src/config.js
@@ -3,4 +3,16 @@
 let config = module.exports = {
   // Beep Boop Persist API provider (beepboop, fs, memory)
   persist_provider: process.env.PERSIST_PROVIDER || 'beepboop',
+
+  validate: () => {
+    let required = ['persist_provider']
+
+    required.forEach((prop) => {
+      if (!config[prop]) {
+        throw new Error(`${prop.toUpperCase()} required but missing`)
+      }
+    })
+    return config
+  }
+
 }
