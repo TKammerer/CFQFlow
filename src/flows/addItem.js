a3f31d94a672b276a792bb97c4b4de0a96b052be
Trent Kammerer <trent.kammerer@aarons.com>
2017-06-06 14:01:07 -0400

    adding auth to review and remove work item commands

--- src/flows/addItem.js
+++ src/flows/addItem.js
@@ -6,37 +6,43 @@ module.exports = (app) => {
 
     slapp.message('remove work item (.*)', ['direct_mention', 'direct_message'], (msg, text, workItemTitle) => {
         slapp.client.users.info({ token: msg.meta.bot_token, user: msg.meta.user_id }, (err, data) => { 
+            kv.get("owners", (err, roleList) => {
+                if (err) return handleError(err, msg)
 
-            kv.get("workItems", (err, dbworkItemList) => {
-            
-                let workItemList = [];
+                if(roleList.indexOf(data.user.name) !== -1){
+                    kv.get("workItems", (err, dbworkItemList) => {  
+                        let workItemList = [];
 
-                if(dbworkItemList != null)
-                    workItemList = dbworkItemList;
+                        if(dbworkItemList != null)
+                            workItemList = dbworkItemList;
 
-                var workItem = workItemList.find(x => x.title === workItemTitle)
+                        var workItem = workItemList.find(x => x.title === workItemTitle)
 
-                if(workItem == null){
-                    msg.say("Cannot find *" + workItemTitle + "*!")
-                    return
-                }
+                        if(workItem == null){
+                            msg.say("Cannot find *" + workItemTitle + "*!")
+                            return
+                        }
 
-                let index = workItemList.indexOf(workItem)
-                if(index !== -1)
-                    workItemList.splice(index, 1);
+                        let index = workItemList.indexOf(workItem)
+                        if(index !== -1)
+                            workItemList.splice(index, 1);
 
-                kv.set("workItems", workItemList, (err) => {
-                    if (err) return handleError(err, msg)
-                    
-                    kv.get("workItems", (err, updatedWorkItemList) => {
-                        if (err) return handleError(err, msg)
-                        
-                    if(updatedWorkItemList == null)
-                        msg.say("Nothing Found!")
-                    else
-                        msg.say("Removed " + workItemTitle + " from current work item list.").say("Current list: ").say(`\`\`\`${JSON.stringify(workItemList)}\`\`\``)
+                        kv.set("workItems", workItemList, (err) => {
+                            if (err) return handleError(err, msg)
+                            
+                            kv.get("workItems", (err, updatedWorkItemList) => {
+                                if (err) return handleError(err, msg)
+                                
+                            if(updatedWorkItemList == null)
+                                msg.say("Nothing Found!")
+                            else
+                                msg.say("Removed " + workItemTitle + " from current work item list.").say("Current list: ").say(`\`\`\`${JSON.stringify(workItemList)}\`\`\``)
+                            })
+                        })
                     })
-                })
+                }
+                else
+                    msg.say("Must be owner!")
             })
         })
     })
@@ -80,107 +86,128 @@ module.exports = (app) => {
         msg.say(`Sounds good! Little description?`).route('new-item-desc', state)
     })
     .route('new-item-desc', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || ''
+        var text = (msg.body.event && msg.body.event.text) || ''
 
-    if (!text) {
-      return msg
-        .say("I'm eagerly awaiting to hear the description.")
-        .route('new-item-desc', state)
-    }
-
-    state.desc = text
-
-    kv.get("workItems", (err, workItemList) => {
-        if (err) return handleError(err, msg)
-
-        let list = [];
-
-        if(workItemList != null){
-            list = workItemList
+        if (!text) {
+        return msg
+            .say("I'm eagerly awaiting to hear the description.")
+            .route('new-item-desc', state)
         }
 
-        list.push(state)
+        state.desc = text
 
-        kv.set("workItems", list, (err) => {
+        kv.get("workItems", (err, workItemList) => {
             if (err) return handleError(err, msg)
+
+            let list = [];
+
+            if(workItemList != null){
+                list = workItemList
+            }
+
+            list.push(state)
+
+            kv.set("workItems", list, (err) => {
+                if (err) return handleError(err, msg)
+            })
         })
-    })
 
-    kv.get("qa reviewers", (err, roleList) => {
-        if (err) return handleError(err, msg)
-
-        msg.say('Thanks!').say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
-
-        roleList.forEach(function(element){
-            msg.say('@'+element + ' *' + state.title + '* changes do not require QA or have automated test coverage?')
-            msg.say('@'+element + ' Please reply "qa review ' + state.title + ' yes/no"')
-        })
-        
-        kv.get("dev reviewers", (err, roleList) => {
+        kv.get("qa reviewers", (err, roleList) => {
             if (err) return handleError(err, msg)
+
+            msg.say('Thanks!').say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
 
             roleList.forEach(function(element){
-                msg.say('@'+element + ' *' + state.title + '* changes constainted to one system?')
-                msg.say('@'+element + ' Please reply "dev review ' + state.title + ' yes/no"')
+                msg.say('@'+element + ' *' + state.title + '* changes do not require QA or have automated test coverage?')
+                msg.say('@'+element + ' Please reply "qa review ' + state.title + ' yes/no"')
+            })
+            
+            kv.get("dev reviewers", (err, roleList) => {
+                if (err) return handleError(err, msg)
+
+                roleList.forEach(function(element){
+                    msg.say('@'+element + ' *' + state.title + '* changes constainted to one system?')
+                    msg.say('@'+element + ' Please reply "dev review ' + state.title + ' yes/no"')
+                })
             })
         })
     })
-  })
 
     slapp.message('(qa|dev) review (.*) (yes|no)', 'mention', (msg, text, role, item, answer) => {
         if(role === 'dev') {
-            if(answer === 'no'){
-                msg.say("Can you please give a quick explanation for the channel?").route('handleDevNo', item)
-            }
-            else{
-                kv.get("workItems", (err, dbworkItemList) => {
-                    var workItem = dbworkItemList.find(x => x.title === item)
+            slapp.client.users.info({ token: msg.meta.bot_token, user: msg.meta.user_id }, (err, data) => { 
+                kv.get("dev reviewers", (err, roleList) => {
+                    if (err) return handleError(err, msg)
 
-                    workItem.devApproved = true;
+                    if(roleList.indexOf(data.user.name) !== -1){
+                        if(answer === 'no'){
+                            msg.say("Can you please give a quick explanation for the channel?").route('handleDevNo', item)
+                        }
+                        else{
+                            kv.get("workItems", (err, dbworkItemList) => {
+                                var workItem = dbworkItemList.find(x => x.title === item)
 
-                    let answerText = "Thanks!";
+                                workItem.devApproved = true;
 
-                    if(workItem.qaApproved) {
-                        workItem.accepted = true;
-                        answerText = "Thanks! *" + item + "* is fully approved!"
-                    }
-                    else{
-                        answerText = "Thanks! *" + item + "* is waiting on QA Review."
-                    }
+                                let answerText = "Thanks!";
 
-                    kv.set("workItems", dbworkItemList, (err) => {
-                        if (err) return handleError(err, msg)
-                        msg.say(answerText)
-                    })
+                                if(workItem.qaApproved) {
+                                    workItem.accepted = true;
+                                    answerText = "Thanks! *" + item + "* is fully approved!"
+                                }
+                                else{
+                                    answerText = "Thanks! *" + item + "* is waiting on QA Review."
+                                }
+
+                                kv.set("workItems", dbworkItemList, (err) => {
+                                    if (err) return handleError(err, msg)
+                                    msg.say(answerText)
+                                })
+                            })
+                        }
+                    }           
+                    else
+                        msg.say("Must be Dev Reviewer!")
                 })
-            }
+            })
         }
         else {
-            if(answer === 'no'){
-                msg.say("Can you please give a quick explanation for the channel?").route('handleQANo', item)
-            }
-            else{
-                kv.get("workItems", (err, dbworkItemList) => {
-                    var workItem = dbworkItemList.find(x => x.title === item)
+            slapp.client.users.info({ token: msg.meta.bot_token, user: msg.meta.user_id }, (err, data) => { 
+                kv.get("owners", (err, roleList) => {
+                    if (err) return handleError(err, msg)
 
-                    workItem.qaApproved = true;
+                    if(roleList.indexOf(data.user.name) !== -1){
 
-                    let answerText = "Thanks!";
+                        if(answer === 'no'){
+                            msg.say("Can you please give a quick explanation for the channel?").route('handleQANo', item)
+                        }
+                        else{
+                            kv.get("workItems", (err, dbworkItemList) => {
+                                var workItem = dbworkItemList.find(x => x.title === item)
 
-                    if(workItem.qaApproved) {
-                        workItem.accepted = true;
-                        answerText = "Thanks! *" + item + "* is fully approved!"
+                                workItem.qaApproved = true;
+
+                                let answerText = "Thanks!";
+
+                                if(workItem.qaApproved) {
+                                    workItem.accepted = true;
+                                    answerText = "Thanks! *" + item + "* is fully approved!"
+                                }
+                                else{
+                                    answerText = "Thanks! *" + item + "* is waiting on Dev Review."
+                                }
+
+                                kv.set("workItems", dbworkItemList, (err) => {
+                                    if (err) return handleError(err, msg)
+                                    msg.say(answerText)
+                                })
+                            })
+                        }
                     }
-                    else{
-                        answerText = "Thanks! *" + item + "* is waiting on Dev Review."
-                    }
-
-                    kv.set("workItems", dbworkItemList, (err) => {
-                        if (err) return handleError(err, msg)
-                        msg.say(answerText)
-                    })
+                    else
+                        msg.say("Must be QA Reviewer!")
                 })
-            }
+            })
         }
     })
 
@@ -201,25 +228,25 @@ module.exports = (app) => {
             })
     })
 
-        slapp.route('handleQANo', (msg, item) => {
-            var text = (msg.body.event && msg.body.event.text) || ''
+    slapp.route('handleQANo', (msg, item) => {
+        var text = (msg.body.event && msg.body.event.text) || ''
 
-            if (!text) {
-                return msg.say("Whoops, I'm still waiting to hear from you.").route('handleQANo', item)
-            }
+        if (!text) {
+            return msg.say("Whoops, I'm still waiting to hear from you.").route('handleQANo', item)
+        }
 
-            kv.get("workItems", (err, dbworkItemList) => {
-                var workItem = dbworkItemList.find(x => x.title === item)
+        kv.get("workItems", (err, dbworkItemList) => {
+            var workItem = dbworkItemList.find(x => x.title === item)
 
-                workItem.qaApproved = false;
-                workItem.rejected = true;
-                workItem.qaReason = text;
+            workItem.qaApproved = false;
+            workItem.rejected = true;
+            workItem.qaReason = text;
 
-                kv.set("workItems", dbworkItemList, (err) => {
-                    if (err) return handleError(err, msg)
-                    msg.say("Thanks!")
-                })
+            kv.set("workItems", dbworkItemList, (err) => {
+                if (err) return handleError(err, msg)
+                msg.say("Thanks!")
             })
+        })
     })
 
     function handleError (err, msg) {
