996b0d2e6f31425f3530b503217199637338fac0
Trent Kammerer <trent.kammerer@aarons.com>
2017-06-08 11:58:01 -0400

    test

--- src/flows/addItem.js
+++ src/flows/addItem.js
@@ -47,7 +47,7 @@ module.exports = (app) => {
         })
     })
 
-    slapp.message('view (all|workable) work items', ['direct_mention', 'direct_message'], (msg, type) => {
+    slapp.message('view (all|workable) work items', ['direct_mention', 'direct_message'], (msg, text, type) => {
         kv.get("workItems", (err, dbworkItemList) => {
             if (err) return handleError(err, msg)
 
