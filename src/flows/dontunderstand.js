40becd3787ad80d4e7a61fcdf6f6fa836984a728
Trent Kammerer <trent.kammerer@aarons.com>
2017-06-22 10:14:25 -0400

    test

--- src/flows/dontunderstand.js
+++ src/flows/dontunderstand.js
@@ -3,11 +3,11 @@
 module.exports = (app) => {
   let slapp = app.slapp
 
-  slapp.message('(.*)', ['direct_message'], (msg) => {    
+  slapp.message('(.*)', ['direct_mention', 'direct_message'], (msg) => {    
     slapp.client.users.info({ token: msg.meta.bot_token, user: msg.meta.user_id }, (err, data) => {
         if (err) return msg.respond(`Sorry, something went wrong. Try again? (${err.message || err})`)
             msg.say("Sorry " + data.user.name + ", I don't understand.");
         })
   })
-  
+
 }
